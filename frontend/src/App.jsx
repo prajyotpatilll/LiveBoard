7b00176079377d38826932a49cdd38d84f7c1219
Prajyot Patil <prajyotbpatil2744@gmail.com>
2025-11-26 11:17:12 +0530

    to test github

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Draw from './pages/Draw'
 import ChatApp from './pages/Chat'
 
 const App = () => {
+  const [board, setBoard] = useState(true);
+  const onClick = () => {
+    setBoard(!board);
+  }
   return (
     <div>
+      {/* <button onClick={onClick}>
+        {board ? "Chat" : "Draw"}
+      </button> */}
       {/* <Draw/> */}
+      {/* {
+        board ? <Draw/> : <ChatApp/>
+      } */}
       <ChatApp/>
     </div>
   )
